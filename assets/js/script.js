2852f013e9365c0521972ef57fc6db173e899190
Natalia García <ngarcia.bustamante@outlook.com>
2024-03-05 10:41:42 -0300

    Se colocan títulos a ejercicios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//EJERCICIO 2//
 //Se crea la variable num1 y se le da el valor ingresado por el usuario
 let num1 = parseInt(prompt("Ingresa un número distinto a 0")); 
 //Se crea la variable num2 y se le da el valor ingresado por el usuario
@@ -5,6 +6,7 @@ let num2 = parseInt(prompt("Ingresa un número distinto a 0"))
 //Se usa la interpolación de variables para distintas operaciones, y un alert con el resultado
 alert (`El resultado de la suma es ${num1+num2}, el resultado de la resta es ${num1-num2}, el resultado de la división es ${num1/num2}, el resultado de la multiplicación es ${num1*num2} y el módulo es ${num1%num2}.`); 
 
+//EJERCICIO 3//
 //Se crea la variable celsius y se le da el valor ingresado por el usuario
 let celsius = parseFloat(prompt("Ingresa la temperatura en ºC"))
 //Se crea la constante kelvin
@@ -14,6 +16,7 @@ const fahrenheit = parseFloat((celsius*9/5)+32)
 //Se crea un alert con ambos resultados
 alert(`La temperatura en Kelvin es de ${kelvin}, la temperatura en Fahrenheit es de ${fahrenheit}.`)
 
+//EJERCICIO 4//
 //Se crea variable dias y se le da el valor ingresado por el usuario
 let dias = parseInt(prompt("Ingresa una cantidad de días para calcular su equivalencia"))
 //Se crea constante año
@@ -25,6 +28,7 @@ const dia = Math.floor((dias%365)%7)
 //Se crea un alert con resultados
 alert(`Equivale a ${año} año(s), ${semanas} semana(s) y ${dia} día(s)`)
 
+//EJERCICIO 5//
 //Se crean las variables nro1 a nro5 y se le da el valor ingresado por el usuario
 let nro1 = parseInt(prompt("Ingresa un número"))
 let nro2 = parseInt(prompt("Ingresa un número"))
