1279ac66524e963956aa737143e56836a29824a6
Natalia García <ngarcia.bustamante@outlook.com>
2024-03-05 10:36:25 -0300

    Se crea respuesta a ejercicio 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
-
+//Se crea la variable num1 y se le da el valor ingresado por el usuario
+let num1 = parseInt(prompt("Ingresa un número distinto a 0")); 
+//Se crea la variable num2 y se le da el valor ingresado por el usuario
+let num2 = parseInt(prompt("Ingresa un número distinto a 0")) 
+//Se usa la interpolación de variables para distintas operaciones, y un alert con el resultado
+alert (`El resultado de la suma es ${num1+num2}, el resultado de la resta es ${num1-num2}, el resultado de la división es ${num1/num2}, el resultado de la multiplicación es ${num1*num2} y el módulo es ${num1%num2}.`); 
 
